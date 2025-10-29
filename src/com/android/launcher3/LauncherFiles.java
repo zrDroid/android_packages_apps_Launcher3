5b9b01fd323704d30a17ce0dcde7cf09e281bf30
zelleb <zelleb@esau.li>
2025-10-29 20:24:46 +0100

    Launcher3: Add 5x10 workspace

--- src/com/android/launcher3/LauncherFiles.java
+++ src/com/android/launcher3/LauncherFiles.java
@@ -23,6 +23,7 @@ public class LauncherFiles {
     public static final String LAUNCHER_4_BY_5_DB = "launcher_4_by_5.db";
     public static final String LAUNCHER_4_BY_6_DB = "launcher_4_by_6.db";
     public static final String LAUNCHER_5_BY_6_DB = "launcher_5_by_6.db";
+    public static final String LAUNCHER_5_BY_10_DB = "launcher_5_by_10.db";
     public static final String LAUNCHER_4_BY_4_DB = "launcher_4_by_4.db";
     public static final String LAUNCHER_3_BY_3_DB = "launcher_3_by_3.db";
     public static final String LAUNCHER_2_BY_2_DB = "launcher_2_by_2.db";
@@ -53,6 +54,7 @@ public class LauncherFiles {
             LAUNCHER_4_BY_5_DB,
             LAUNCHER_4_BY_6_DB,
             LAUNCHER_5_BY_6_DB,
+            LAUNCHER_5_BY_10_DB,
             LAUNCHER_4_BY_4_DB,
             LAUNCHER_3_BY_3_DB,
             LAUNCHER_2_BY_2_DB,
